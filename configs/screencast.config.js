9f8fda4af3c5074e1c0239a3f6b39a79989b0df4
Lance Tipton <lancetipton04@gmail.com>
2022-08-05 16:41:00 -0700

    fix: add GB_NO_VNC_SERVER_HOST env

--- configs/screencast.config.js
+++ configs/screencast.config.js
@@ -1,15 +1,21 @@
 const { toBool } = require('@keg-hub/jsutils')
 
 const {
-  GB_VNC_ACTIVE,
-  DISPLAY = ':0.0',
-  GB_SC_PORT = 7006,
-  GB_NO_VNC_PORT = 26369,
   GB_SC_HOST,
-  GB_VNC_VIEW_HEIGHT = 900,
-  GB_VNC_VIEW_WIDTH = 1440,
-  GB_VNC_SERVER_PORT = 26370,
-  GB_VNC_SERVER_HOST = GB_SC_HOST,
+  GB_BE_HOST,
+  GB_VNC_ACTIVE,
+  DISPLAY=':0.0',
+  GB_SC_PORT=7006,
+
+  GB_NO_VNC_PORT=26369,
+  GB_NO_VNC_PATH=`/novnc`,
+  GB_NO_VNC_PROTOCOL=`ws`,
+  GB_NO_VNC_SERVER_HOST=GB_BE_HOST,
+
+  GB_VNC_VIEW_HEIGHT=900,
+  GB_VNC_VIEW_WIDTH=1440,
+  GB_VNC_SERVER_PORT=26370,
+  GB_VNC_SERVER_HOST=GB_SC_HOST,
 } = process.env
 
 
@@ -21,11 +27,13 @@ const screenDims = {
 const screencastConfig = {
   // Set if the screencast is active or not
   active: toBool(GB_VNC_ACTIVE),
+  // TODO - This should be moved from screencast to server config
   // Proxy settings, for connecting the backend API to the noVNC server
   proxy: {
-    path: '/novnc',
+    path: GB_NO_VNC_PATH,
     port: GB_NO_VNC_PORT,
-    host: GB_VNC_SERVER_HOST,
+    host: GB_NO_VNC_SERVER_HOST,
+    protocol: GB_NO_VNC_PROTOCOL,
   },
   // Uses to start separate screencast API
   server: {
