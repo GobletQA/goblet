4381a3d0193e4adf1bf70ed731ff5cbf59f31ce2
Lance Tipton <lancetipton04@gmail.com>
2022-08-07 18:43:39 -0700

    fix: default vnc to active in devspace

--- scripts/js/resolveDSEnvs.js
+++ scripts/js/resolveDSEnvs.js
@@ -44,11 +44,11 @@ const buildEnvs = (envs, list) => {
 }
 
 const [repo] = process.argv.slice(2)
-const isFE = repo === `frontend`
-const envs = resolveValues()
 let dsEnvs = addEnv(`GB_SUB_REPO`, repo)
+dsEnvs += addEnv(`GB_VNC_ACTIVE`, `"true"`)
 
+const envs = resolveValues()
 dsEnvs+= buildEnvs(envs, defEnvs)
-dsEnvs+= isFE ? buildEnvs(envs, feKeys) : buildEnvs(envs, beKeys)
+dsEnvs+= repo === `frontend` ? buildEnvs(envs, feKeys) : buildEnvs(envs, beKeys)
 
 process.stdout.write(dsEnvs)
