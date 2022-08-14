0279a9629b5eb597e381dd6a2f8b369c2653ec9b
Lance Tipton <lancetipton04@gmail.com>
2022-08-14 05:05:42 -0700

    refactor(be): migrate typescript

--- repos/backend/src/constants/constants.js
+++
@@ -1,6 +0,0 @@
-const { keyMap } = require('@keg-hub/jsutils')
-
-module.exports = {
-  NOT_REPLACE: '( not)*',
-  ...keyMap(['NOT_PARAMETER', `NOT_INDEX`, 'PARAMETER'], true),
-}
---
+++ repos/backend/src/constants/constants.ts
@@ -0,0 +1,5 @@
+
+export const NOT_REPLACE = '( not)*'
+export const NOT_PARAMETER = `NOT_PARAMETER`
+export const NOT_INDEX = `NOT_INDEX`
+export const PARAMETER = `PARAMETER`
