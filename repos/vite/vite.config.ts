0f7c15c2d1e4ae59c855aa83d04f0f223363e9ba
Lance Tipton <lancetipton04@gmail.com>
2022-09-16 18:06:17 -0700

    feat(vite): Setup vite to replace fe

--- repos/vite/vite.config.ts
+++ repos/vite/vite.config.ts
@@ -1,8 +1,18 @@
+import path from 'path'
 import { defineConfig } from 'vite'
+import mkcert from'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
+
+process.env.PLUGIN_DATA_DIR = path.join(__dirname, `../../certs`)
+
+const pathResolve = (dir: string) => resolve(__dirname, '.', dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()]
+  server: {
+    https: true
+  },
+  plugins: [tsconfigPaths(), react(), mkcert()]
 })
