2a842b014eb82dbe9e8afe1f7e364dbbc6bee186
Lance Tipton <lancetipton04@gmail.com>
2022-09-18 17:01:05 -0700

    feat(vite): cleanup header

--- repos/vite/src/components/header/header.tsx
+++ repos/vite/src/components/header/header.tsx
@@ -1,59 +1,34 @@
 import * as React from 'react'
-import { SideNav } from '@constants/Nav'
-import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import { AppHeader } from './Header.styled'
 
 import { Logo } from './Logo'
-import { Tabs } from './Tabs'
 import { Settings } from './Settings'
 
 type THeaderProps = {
-  pages?: string[]
   settings?: string[]
 }
 
 export const Header = (props:THeaderProps) => {
   const {
-    pages=[],
     settings=['Profile', 'Account', 'Dashboard', 'Logout']
   } = props
-  
-  
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null)
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget)
-  }
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget)
-  }
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null)
   }
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null)
   }
 
-  const onTabClick = (event:any, tab:string) => {
-    console.log(`------- tab -------`)
-    console.log(tab)
-  }
-
   return (
     <AppHeader position="fixed">
       <Toolbar disableGutters>
-        <Tabs
-          tabs={pages}
-          anchorEl={anchorElNav}
-          onTabClick={onTabClick}
-          onOpenNav={handleOpenNavMenu}
-          onCloseTab={handleCloseNavMenu}
-        />
-        <Logo mobile />
+        <Box sx={{ flex: 1 }} />
         <Settings
           settings={settings}
           anchorEl={anchorElUser}
