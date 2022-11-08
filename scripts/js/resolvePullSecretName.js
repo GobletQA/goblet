64e2393f5361793deca00006346e92deb13ef858
Lance Tipton <lancetipton04@gmail.com>
2022-11-07 18:55:50 -0700

    feat: add script to pull the imagePull secret from kubernetes * Looks for first secret that starts with devspace * May need some cleanup, but works for now

---
+++ scripts/js/resolvePullSecretName.js
@@ -0,0 +1,24 @@
+/**
+ * Used by devspace in the devspace.yml to dynamically pull secrets for the application deployment
+ * Allows loading them from the container/values*.yaml based on NODE_ENV
+ * Test =>  node scripts/js/resolvePullSecrets.js
+ */
+const { runCmd } = require('@keg-hub/cli-utils')
+
+;(async () => {
+  const { data, error, exitCode } = await runCmd(`kubectl`, [
+    `get`,
+    `secrets`,
+    `-o`,
+    `json`
+  ], { exec: true })
+  
+  if(exitCode)
+    throw new Error(error || `The "kubectl" exited with a non-zero exit code, but provided no error.`)
+
+  const secrets = JSON.parse(data)
+  const found = secrets.items.find(secret => secret.metadata.name.startsWith(`devspace`))
+  if(!found) throw new Error(`Could not find a valid devspace secret.`)
+
+  process.stdout.write(found.metadata.name)
+})()
