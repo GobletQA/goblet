105eba0f0558eace73d4d95f8391907d122436bf
Lance Tipton <lancetipton04@gmail.com>
2022-12-23 05:34:11 -0700

    fix: loadEnvs require

--- tasks/utils/envs/loadEnvs.ts
+++ tasks/utils/envs/loadEnvs.ts
@@ -5,7 +5,7 @@ import { createRequire } from 'module'
 const TasksLoadEnvs = (...args:any[]) => {
   const shareLoc = path.resolve(path.join(repos.shared, `src/utils`))
   const require = createRequire(shareLoc)
-  const { loadEnvs } = require(`./utils/loadEnvs.js`)
+  const { loadEnvs } = require(`./utils/loadEnvs`)
 
   return loadEnvs(...args)
 }
