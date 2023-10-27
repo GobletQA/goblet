4c55fe58409c1b12a0b45364f94a8d3055080507
Lance Tipton <lancetipton04@gmail.com>
2023-10-26 19:05:27 -0700

    feat: fix backend image (#31)

--- repos/testUtils/src/steps/when/setSelectOption.ts
+++ repos/testUtils/src/steps/when/setSelectOption.ts
@@ -2,7 +2,11 @@ import type { TStepCtx } from '@GTU/Types'
 
 import { When } from '@GTU/Parkin'
 import { saveWorldData, saveWorldLocator } from '@GTU/Support/helpers'
-import { ExpressionKinds, ExpressionTypes } from '@GTU/Constants'
+import {
+  ExpressionKinds,
+  ExpressionTypes,
+  ExpressionElements
+} from '@GTU/Constants'
 
 /**
  * Expects the element matching `selector` and selects the option(s) with the label === `data`
@@ -12,12 +16,10 @@ import { ExpressionKinds, ExpressionTypes } from '@GTU/Constants'
 export const setSelectOption = async (
   selector:string,
   data:string,
-  key:string = 'label',
+  key:string,
   ctx:TStepCtx
 ) => {
   const { world } = ctx
-
-  // Defaults to use label if no 'by' key exists
   const options = data.split(',').map(value => ({ [key]: value }))
   const element = await saveWorldLocator({ selector, world }, ctx)
   const resp = await element.selectOption(options)
@@ -28,13 +30,15 @@ export const setSelectOption = async (
 }
 
 When(`I set the select {string} to {string} by {string}`, setSelectOption, {
+  name: `Select by value or label`,
   module: `setSelectOption`,
-  description: `Locates a select element by selector and selects specified options.  Can specify options by option label or option value.`,
+  description: `Locates a native HTML select element by selector and selects specified options. Can specify the option to select by label (option text) or value attribute.`,
   expressions: [
     {
       type: ExpressionTypes.string,
       kind: ExpressionKinds.element,
-      description: `The selector for the select element.  Selector must be specific enough to locate a single element.`,
+      kindRef: ExpressionElements.select,
+      description: `The selector for the native HTML select element.`,
       example: `select[name='unique_name']`,
     },
     {
@@ -42,15 +46,14 @@ When(`I set the select {string} to {string} by {string}`, setSelectOption, {
       // TODO: add input for tags?
       // kind: ExpressionKinds.tags,
       kind: ExpressionKinds.text,
-      description: `Comma delimited list of option(s) to select.  Can be option labels or values.`,
+      description: `Comma delimited list of option(s) to select. Can be option labels or values.`,
       example: `California, Oregon, Washington`,
     },
     {
+      options: [`value`, `label`],
       type: ExpressionTypes.string,
-      // TODO: add enum for option label || value
-      // kind: ExpressionKinds.optionEnum,
-      kind: ExpressionKinds.text,
-      description: `Valid options are \'label\' or \'value\' only.`,
+      kind: ExpressionKinds.options,
+      description: `How to identify the option to select, by label (option text) or value attribute.`,
       example: `value`,
     },
   ],
