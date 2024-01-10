3c2898cfcf3435b9789ac871200bf11be37acd85
Lance Tipton <lancetipton04@gmail.com>
2024-01-10 16:22:27 -0700

    fix: pod annotations check

--- repos/conductor/src/controller/kube/pod/getPodAnnotations.ts
+++ repos/conductor/src/controller/kube/pod/getPodAnnotations.ts
@@ -5,18 +5,18 @@ import { noOpObj } from '@keg-hub/jsutils/noOpObj'
 import { PodAnnotations, ConductorUserHashLabel } from '@GCD/constants'
 
 export const getPodAnnotations = (pod:TPod) => {
-  const name = pod.metadata.annotations[PodAnnotations.component]
-  const userHash = pod.metadata.annotations[ConductorUserHashLabel]
+  const name = pod?.metadata?.annotations?.[PodAnnotations.component]
+  const userHash = pod?.metadata?.annotations?.[ConductorUserHashLabel]
   try {
     const ports = userHash
-      ? JSON.parse(pod.metadata.annotations[PodAnnotations.ports])
+      ? JSON.parse(pod?.metadata?.annotations?.[PodAnnotations.ports])
       : noOpObj
 
     return { ports, name, userHash }
   }
   catch(err){
     Logger.error(`Pod missing ports annotation.`)
-    Logger.info(pod.metadata.annotations)
+    Logger.info(pod?.metadata?.annotations)
     return { name, userHash }
   }
 }
